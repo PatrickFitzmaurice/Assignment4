a03f142d2c1c4ab89b39d003a69ff1e84bc196ba
Patrick Fitzmaurice <fitz0251@algonquinlive.com>
2020-12-06 17:09:45 -0500

    Added programmer comments, and starter code to method main.
    Signed-off-by: Patrick Fitzmaurice <fitz0251@algonquinlive.com>

--- Assignment 4/src/Assignment04PatrickFitzmaurice.java
+++ Assignment 4/src/Assignment04PatrickFitzmaurice.java
@@ -1,8 +1,19 @@
+/*
+ * Assessment: Assignment 04
+ * Student Name: Patrick Fitzmaurice
+ * Due Date: December 6th, 2020
+ * Description: Using Git within Eclipse 
+ * Professor Name: Professor Name: Mohammad Patoary
+ */
 
 public class Assignment04PatrickFitzmaurice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] primes = { 2, 3, 5, 7 };
+		for(int index = 0; index <= primes.length; index++) {
+			System.out.println(primes[index]);
+		}
+		System.out.println("Program by Patrick Fitzmaurice");
 
 	}
 
